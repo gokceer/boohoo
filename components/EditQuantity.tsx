33e66010b234fe49dcbe2bbc8f16e96e7f92f990
gokceer <gkcer@hotmail.com>
2022-11-28 01:28:57 +0300

    Add comment in EditQuantity component

--- components/EditQuantity.tsx
+++ components/EditQuantity.tsx
@@ -14,21 +14,24 @@ type EditQuantityPropsType = {
 }
 
 const EditQuantity = ({ itemInfo }: EditQuantityPropsType) => {
+  // Take product item info from ProductRowItem
   const { id, colour, name, price, img } = itemInfo
   const items = useAppSelector((state) => selectCartItemsWithId(state, id));
   const dispatch = useAppDispatch();
 
+  // Adds product to the shopping cart using addToCart reducer
   const addItemsToCart = () => {
     dispatch(addToCart({ id, colour, name, price, img }))
   }
 
+  // Remove one item with specified id from the cart
   const removeItemFromCart = () => {
     if (!(items.length > 0)) return;
 
     dispatch(removeFromCart({ id }));
   };
 
-
+  // Add/remove item and show quantity
   return (
     <View>
       <View style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
